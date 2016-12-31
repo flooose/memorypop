2a42b52b8e6f4e7ca5cf28d4e90af129c5158c14
christopher floess <chris.floess@jjff.de>
2016-12-31 15:58:16 +0100

    allow displaying defintions and switching to next word

--- app/src/main/java/de/jjff/flooose/memorypop/NewEntryActivity.java
+++ app/src/main/java/de/jjff/flooose/memorypop/NewEntryActivity.java
@@ -2,27 +2,49 @@ package de.jjff.flooose.memorypop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class NewEntryActivity extends AppCompatActivity {
 
+    private StaticDictionaryService.DictionaryEntry currentEntry;
+    private StaticDictionaryService.VocabDictionary currentDictionary;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_entry);
 
-        StaticDictionaryService staticJsonService = new StaticDictionaryService();
-        StaticDictionaryService.VocabDictionary dictionary = StaticDictionaryService.getDefaultDictionary();
+        currentDictionary = StaticDictionaryService.getDefaultDictionary();
 
-        int currentEntryIndex = new Random().nextInt(dictionary.dictionaryEntries.length);
+        displayRandomWord();
+    }
 
-        StaticDictionaryService.DictionaryEntry currentEntry = dictionary.dictionaryEntries[currentEntryIndex];
+    public void toggleDefinition(View view) {
+        TextView wordView = (TextView) findViewById(R.id.entry_word);
+        Button button = (Button) findViewById(R.id.reveal_definition);
+        if(button.getText().equals(getString(R.string.reveal_definition))) {
+            button.setText(getString(R.string.ok));
+            wordView.setText(currentEntry.mDefinition);
+        } else {
+            button.setText(getString(R.string.reveal_definition));
+            wordView.setText(currentEntry.mWord);
+
+        }
+    }
+
+    public void nextWord(View view) {
+        displayRandomWord();
+    }
+
+    private void displayRandomWord() {
+        int currentEntryIndex = new Random().nextInt(currentDictionary.dictionaryEntries.length);
+        currentEntry = currentDictionary.dictionaryEntries[currentEntryIndex];
 
         TextView wordView = (TextView) findViewById(R.id.entry_word);
         wordView.setText(currentEntry.mWord);
-        TextView definitionView = (TextView) findViewById(R.id.entry_definition);
-        definitionView.setText(currentEntry.mDefinition);
     }
 }
