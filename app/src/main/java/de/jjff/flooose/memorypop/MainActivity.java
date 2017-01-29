c36b2916cedf16caaa5cccafea579fdc1bffa2c9
christopher floess <chris.floess@jjff.de>
2017-01-29 19:07:09 +0100

    Add MainActivity as entry point to app

---
+++ app/src/main/java/de/jjff/flooose/memorypop/MainActivity.java
@@ -0,0 +1,66 @@
+package de.jjff.flooose.memorypop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static de.jjff.flooose.memorypop.R.id.newWordLayout;
+import static de.jjff.flooose.memorypop.R.id.playLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View playButton;
+    private View newWordButton;
+
+    @BindView(R.id.startLayout) View startLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ButterKnife.bind(this);
+
+        playButton = startLayout.findViewById(R.id.play_button);
+        newWordButton = startLayout.findViewById(R.id.new_word_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                playLayout.setVisibility(View.VISIBLE);
+//                newWordLayout.setVisibility(View.GONE);
+            }
+        });
+
+        newWordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NewEntryActivity.class);
+                startActivity(intent);
+//                newWordLayout.setVisibility(View.VISIBLE);
+//                playLayout.setVisibility(View.GONE);
+
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/de/jjff/flooose/memorypop/views/WordEntryLayout.java
@@ -0,0 +1,27 @@
+package de.jjff.flooose.memorypop.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by chris on 05.01.17.
+ */
+
+public class WordEntryLayout extends LinearLayout {
+    public WordEntryLayout(Context context) {
+        super(context);
+    }
+
+    public WordEntryLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WordEntryLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public WordEntryLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
